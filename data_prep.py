108c9314aabd2b0f4e795489aeed8a057745708b
John Valen <40368715+johnvalen1@users.noreply.github.com>
2019-11-09 15:55:58 -0500

    Update data_prep.py

--- data_prep.py
+++ data_prep.py
@@ -2,9 +2,7 @@ import glob
 import cv2
 import fnmatch
 import numpy as np
-import CNN_model
-
-
+#import CNN_model
 
 #this is a list with ALL images...
 imagePatches = glob.glob("C:/Users/John/Desktop/Research Project/Binary Project/Breast Cancer Data/**/*.png", recursive=True)
@@ -18,32 +16,38 @@ classZero = fnmatch.filter(imagePatches, patternZero)
 #images with label 1 == cancer
 classOne = fnmatch.filter(imagePatches, patternOne)
 
-def process_selected_images(n,p):
+def select_training_images(n,p):
+    '''
+    Creates a list of randomly selected images (each element has name of the form "*class1.png" or
+    "*class0.png" and there are n images with proportion p of them cancerous)
+    '''
+    num_cancer = int(n*p)
+    num_healthy = int(n*(1-p))
+    #num_cancer + num_healthy = n
+    np.random.seed(0)
+    randomly_selected_cancer_images = list(np.random.choice(classOne, num_cancer))
+    np.random.seed(0)
+    randomly_selected_healthy_images = list(np.random.choice(classZero, num_healthy))
+
+    training_set = randomly_selected_cancer_images + randomly_selected_healthy_images
+    return training_set
+
+
+def process_training_images_and_assign_labels(training_set):
     '''
     Returns two arrays:
-        x is an array of resized images respecting the given proportion (i.e. imbalance scheme)
-        y is an array of labels
-        0 <= lowerIndex <= upperIndex <= len(imagePatches) = 277,524
+        processed_x is an array of resized images (numpy arrays) respecting the
+        given proportion p (i.e. imbalance scheme)
+        y is an array of labels (0 or 1)
+        It resizes images to 50x50x3 (RGB channel, not all images are in this layout)
+        It normalizes the x-image pixel values
     '''
     height = 50
     width = 50
     channels = 3
-    x = [] #list to store image data
-    y = [] #list to store corresponding class
-    
-    num_cancer = int(n*p)
-    num_healthy = int(n*(1-p))
-    print(num_cancer, num_healthy)
-    #num_cancer + num_healthy = n
-     #for the same selection every time
-    np.random.seed(0)
-    randomly_selected_cancer_images = list(np.random.choice(classOne, num_cancer))
-     #for the same selection every time
-    np.random.seed(0)
-    randomly_selected_healthy_images = list(np.random.choice(classZero, num_healthy))
-    # random_images = np.random.choice(imagePatches, n, replace=False)#without replacement.
-    random_images = randomly_selected_cancer_images + randomly_selected_healthy_images
-    for img in random_images:
+    x = []
+    y = []
+    for img in training_set:
         full_size_image = cv2.imread(img)
         image = (cv2.resize(full_size_image, (width,height), interpolation=cv2.INTER_CUBIC))
         x.append(image)
@@ -54,26 +58,63 @@ def process_selected_images(n,p):
         else:
             return
     x = np.array(x)
-    x = X.astype(np.float32)
-    x /= 255.
-    return x,y
+    processed_x = x.astype(np.float32)
+    processed_x /= 255.
+    return processed_x,y
+
+
+def select_test_images(training_images, p = 0.5):
+    '''
+    Given the training set, this function will randomly select a test set of size k according to
+    a train-test split of 90-10 such that NONE of the images chosen are in the training set. The default proportion is 0.5
+    so that the test set is balanced.
+    '''
+    test_size = int(len(training_images)/0.9) - len(training_images)
+    num_cancer_test = int(test_size*p) #how many cancer images in the test set
+    num_healthy_test = int(test_size*(1-p)) #how many healthy images in the test set
+    #num_cancer + num_healthy = k
+    all_images = set(imagePatches)
+    training_data = set(training_images)
+    non_training_data = all_images - training_data #data without the training set
+    classZero_test = fnmatch.filter(imagePatches, patternZero) #all healthy images in this set
+    classOne_test = fnmatch.filter(imagePatches, patternOne) #all cancer images in this set
+
+    np.random.seed(0)
+    randomly_selected_test_cancer_images = list(np.random.choice(classOne_test, num_cancer_test))
+    np.random.seed(0)
+    randomly_selected_test_healthy_images = list(np.random.choice(classZero_test, num_healthy_test))
+    test_set = randomly_selected_test_cancer_images + randomly_selected_test_healthy_images
+    return test_set
+
+def process_test_images(test_images):
+    '''
+    Takes in the given test_images list which consists of image names in string form
+    and converts them to numpy arrays such that the classifier can make predictions on them.
+    '''
+
+    height = 50
+    width = 50
+    channels = 3
+    x=[]
+
+    for img in test_images:
+        full_size_image = cv2.imread(img)
+        image = (cv2.resize(full_size_image, (width,height), interpolation=cv2.INTER_CUBIC))
+        x.append(image)
+    x = np.array(x)
+    processed_test_image = x.astype(np.float32)
+    processed_test_image /= 255.
+    return processed_test_image
 
 
 if __name__=="__main__":
-    #We can select the size of the training set based on indexing. Between m and n
-    sample_sizes = [5000, 10000, 20000] #should be linearly scheduled?
-    proportions = [0.01, 0.05, 0.1, 0.25, 0.5] #should be linearly scheduled?
-    test_size = 0.15
 
-    #now get the training and test data:
-    from sklearn.model_selection import train_test_split
-
-    for sample_size in sample_sizes:
-        for prop in proportions:
-            X, Y = process_selected_images(sample_size, prop)
-            X_train, X_test, y_train, y_test = train_test_split(X,Y,test_size=test_size)
-            #fit the model
-
-            #acquire accuracy metrics
-            
-            #save results in a CSV, row-by-row
+    # for sample_size in sample_sizes:
+    #     for prop in proportions:
+    #         X, Y = select_training_images(sample_size, prop)
+    #
+    #         #fit the model
+    #
+    #         #acquire accuracy metrics
+    #
+    #         #save results in a CSV, row-by-row
